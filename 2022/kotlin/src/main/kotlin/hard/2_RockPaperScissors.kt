6a3db3c37a84e1dac68479e39543b3ebea9115ef
Kryštof Gärtner <krystofgartner@seznam.cz>
2022-12-02 14:13:56 +0100

    2022-2 + Kotlin project

---
+++ 2022/kotlin/src/main/kotlin/hard/2_RockPaperScissors.kt
@@ -0,0 +1,47 @@
+package hard
+
+enum class Shape(val score: Int) {
+    ROCK(1), PAPER(2), SCISSORS(3);
+
+    fun losingShape(): Shape{
+        return when(this){
+            ROCK -> SCISSORS
+            PAPER -> ROCK
+            SCISSORS -> PAPER
+        }
+    }
+
+    fun winningShape(): Shape {
+        return when(this){
+            ROCK -> PAPER
+            PAPER -> SCISSORS
+            SCISSORS -> ROCK
+        }
+    }
+}
+
+object Factory{
+    fun getShape(a: String): Shape = when(a){
+        "A" -> Shape.ROCK
+        "B" -> Shape.PAPER
+        "C" -> Shape.SCISSORS
+        else -> throw Exception()
+    }
+}
+
+// harder
+fun main(){
+    val lines = generateSequence { readLine() }
+    var score = 0
+    lines.forEach {
+        val (a, me) = it.split(" ")
+        val enemy = Factory.getShape(a)
+        score += when(me){
+            "X" -> 0 + enemy.losingShape().score // lose
+            "Y" -> 3 + enemy.score // draw
+            "Z" -> 6 + enemy.winningShape().score // win
+            else -> throw Error()
+        }
+    }
+    println(score)
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # advent-of-code
+Solutions for Advent of code puzzles
+
+[More info...](https://adventofcode.com/2022/about)
