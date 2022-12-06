f74936ab5def0bb40125f22d0abff7b324f1541b
lemonek <krystofgartner@seznam.cz>
2022-12-06 15:15:36 +0100

    2022-5 refactored2

--- 2022/5_Stack.kts
+++ 2022/5_Stack.kts
@@ -3,34 +3,26 @@
 val stacks = Array(9) { ArrayDeque<Char>() }
 
 generateSequence { readLine() }.forEach { line ->
-    if('[' in line){
-        line.forEachIndexed() { i, char -> if (char == '[') stacks[i / 4].addFirst(line[i + 1]) }
-    } else if( line.startsWith("move")){
-        val words = line.split(' ')
-        part2(stacks, )
-    }
+    if('[' in line)
+        line.forEachIndexed { i, char -> if (char == '[') stacks[i / 4].addFirst(line[i + 1]) }
+    else if( line.startsWith("move"))
+        part1(Command.fromLine(line, stacks))
 }
 stacks.forEach { print(it.last()) }
 
-data class Command(val moveTimes: Int, val from: Int, val to: Int)
+fun part1(cmd: Command) = repeat(cmd.moveTimes){ cmd.stackTo.addLast(cmd.stackFrom.removeLast()) }
 
-Object CommandFactory {
-    fun fromLine(line: String): Command = {
-        val words = line.split(' ')
-        return Command(words[1].toInt(), words[3].toInt(), words[5].toInt())
-    }
-}
-
-fun part1(stacks: Array<ArrayDeque<Char>>, cmd: Command) {
-    for (i in 0 until moveTimes)
-        stacks[to - 1].addLast(stacks[from - 1].removeLast())
-}
-
-
-fun part2(stacks: Array<ArrayDeque<Char>>, cmd: Command) {
+fun part2(cmd: Command) {
     val tmpStack = ArrayDeque<Char>()
-    for (i in 0 until moveTimes)
-        tmpStack.addLast(stacks[from - 1].removeLast())
-    for(i in 0 until tmpStack.count())
-        stacks[to - 1].addLast(tmpStack.removeLast())
+    repeat(cmd.moveTimes){ tmpStack.addLast(cmd.stackFrom.removeLast()) }
+    while(!tmpStack.isEmpty()) { cmd.stackTo.addLast(tmpStack.removeLast()) }
+}
+
+data class Command(val moveTimes: Int, val stackFrom: ArrayDeque<Char>, val stackTo: ArrayDeque<Char>){
+    companion object {
+        fun fromLine(line: String, stacks: Array<ArrayDeque<Char>>): Command {
+            val words = line.split(' ')
+            return Command(words[1].toInt(), stacks[words[3].toInt()-1], stacks[words[5].toInt()-1])
+        }
+    }
 }
