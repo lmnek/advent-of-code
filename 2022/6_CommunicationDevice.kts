0fd7b87072288709411b0df3507c47860f3dad2b
lemonek <krystofgartner@seznam.cz>
2022-12-06 14:41:07 +0100

    2022-6

---
+++ 2022/6_CommunicationDevice.kts
@@ -0,0 +1,13 @@
+//https://adventofcode.com/2022/day/6#part2
+val bufferLength = 14
+
+val buffer = arrayOfNulls<Char>(bufferLength)
+var counter = 0
+for(char in readLine()!!){
+    buffer[counter % bufferLength] = char
+    if(counter >= bufferLength - 1 && buffer.distinct().size == bufferLength)
+        break
+    counter+=1
+}
+println(counter + 1)
+
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # advent-of-code
 Solutions for Advent of code puzzles
 
-[More info...](https://adventofcode.com/2022/about)
+## 2022
+- Day 1 -> Python
+- Days 2-25 -> Kotlin
+
+https://adventofcode.com/2022/about
