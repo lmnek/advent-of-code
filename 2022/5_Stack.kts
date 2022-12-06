53b6ddfce393a5303eedea71300cc86aaa586734
lemonek <krystofgartner@seznam.cz>
2022-12-06 15:14:16 +0100

    2022-5 refactored

--- 2022/5_Stack.kts
+++ 2022/5_Stack.kts
@@ -7,18 +7,27 @@ generateSequence { readLine() }.forEach { line ->
         line.forEachIndexed() { i, char -> if (char == '[') stacks[i / 4].addFirst(line[i + 1]) }
     } else if( line.startsWith("move")){
         val words = line.split(' ')
-        part2(stacks, words[1].toInt(), words[3].toInt(), words[5].toInt())
+        part2(stacks, )
     }
 }
 stacks.forEach { print(it.last()) }
 
-fun part1(stacks: Array<ArrayDeque<Char>>, moveTimes: Int, from: Int, to: Int) {
+data class Command(val moveTimes: Int, val from: Int, val to: Int)
+
+Object CommandFactory {
+    fun fromLine(line: String): Command = {
+        val words = line.split(' ')
+        return Command(words[1].toInt(), words[3].toInt(), words[5].toInt())
+    }
+}
+
+fun part1(stacks: Array<ArrayDeque<Char>>, cmd: Command) {
     for (i in 0 until moveTimes)
         stacks[to - 1].addLast(stacks[from - 1].removeLast())
 }
 
 
-fun part2(stacks: Array<ArrayDeque<Char>>, moveTimes: Int, from: Int, to: Int) {
+fun part2(stacks: Array<ArrayDeque<Char>>, cmd: Command) {
     val tmpStack = ArrayDeque<Char>()
     for (i in 0 until moveTimes)
         tmpStack.addLast(stacks[from - 1].removeLast())
