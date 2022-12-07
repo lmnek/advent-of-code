d757a9513f2661fc41d950fc7986d5b4856e9774
Kryštof Gärtner <krystofgartner@seznam.cz>
2022-12-07 23:39:36 +0100

    2022-6 refactored

--- 2022/6_CommunicationDevice.kts
+++ 2022/6_CommunicationDevice.kts
@@ -1,13 +1,19 @@
 //https://adventofcode.com/2022/day/6#part2
-val bufferLength = 14
 
-val buffer = arrayOfNulls<Char>(bufferLength)
-var counter = 0
-for(char in readLine()!!){
-    buffer[counter % bufferLength] = char
-    if(counter >= bufferLength - 1 && buffer.set().size == bufferLength)
-        break
-    counter+=1
+println(betterSolution(14))
+
+fun betterSolution(windowSize: Int) = readln().withIndex().windowed(windowSize, 1).first{
+        window -> window.map{ it.value }.toSet().size == windowSize }.last().index + 1
+
+fun firstSolution(bufferLength: Int): Int {
+    val buffer = arrayOfNulls<Char>(bufferLength)
+    var counter = 0
+    for(char in readLine()!!){
+        buffer[counter % bufferLength] = char
+        if(counter >= bufferLength - 1 && buffer.toSet().size == bufferLength)
+            break
+        counter+=1
+    }
+    return counter + 1
 }
-println(counter + 1)
 
