efc7b4cc55ae0b482ea278acc1769e571080af63
Kryštof Gärtner <krystofgartner@seznam.cz>
2022-12-13 21:32:28 +0100

    2022-13

--- 2022/13_Compare.kts
+++ 2022/13_Compare.kts
@@ -1,5 +1,23 @@
 // https://adventofcode.com/2022/day/13
-import kotlin.math.min
+
+val components: List<Component> = generateSequence(::readLine)
+    .filter { it.isNotEmpty() }.map{ it.toComponent() }.toList()
+
+println("Part1: ${part1(components)}")
+println("Part2: ${part2(components)}")
+
+fun part1(components: List<Component>): Int = components
+    .chunked(2)
+    .mapIndexedNotNull{ i, (o1, o2) -> if(o1 <= o2) i + 1 else null }
+    .sum()
+
+fun part2(components: List<Component>): Int{
+    val dividers = arrayListOf("[[2]]".toComponent(), "[[6]]".toComponent())
+    val newComponents = components.toMutableList()
+    newComponents.addAll(dividers)
+    newComponents.sort()
+    return (newComponents.indexOf(dividers[0]) + 1) * (newComponents.indexOf(dividers[1]) + 1)
+}
 
 // composite design pattern + comparable interface
 sealed interface Component : Comparable<Component>{
@@ -7,17 +25,24 @@ sealed interface Component : Comparable<Component>{
 }
 
 class Composite(private val components: List<Component>) : Component {
-    override fun compareTo(other: Component): Int = when(other) {
-        is Number -> this.compareTo(Composite(listOf(other)))
-        is Composite -> {
-            var res = 0
-            for(i in 0 until min(this.components.size, other.components.size)){
-                res = this.components[i].compareTo(other.components[i])
-                if(res != 0) break
+    override fun compareTo(other: Component): Int {
+        return when (other) {
+            is Number -> this.compareTo(Composite(listOf(other)))
+            is Composite -> {
+                var result: Int?
+                val iter1 = this.components.iterator()
+                val iter2 = other.components.iterator()
+                while (iter1.hasNext() && iter2.hasNext()) {
+                    result = iter1.next().compareTo(iter2.next())
+                    if (result != 0) return result
+                }
+                if (!iter1.hasNext() && !iter2.hasNext()) 0
+                else if (iter1.hasNext()) 1
+                else if (iter2.hasNext()) -1
+                else throw Error()
             }
-            res
+            else -> throw Error()
         }
-        else -> 0
     }
 }
 
@@ -25,29 +50,32 @@ class Number(private val value: Int) : Component {
     override fun compareTo(other: Component): Int = when(other) {
         is Number -> value.compareTo(other.value)
         is Composite -> Composite(listOf(this)).compareTo(other)
-        else -> 0
+        else -> throw Error()
     }
 }
 
+// input parser
 fun String.toComponent(): Component {
-    return if (startsWith('[')) {
-        println(substringBeforeLast(']').substringAfter('[').split(','))
-        Composite(substringBeforeLast(']').substringAfter('[')
-            .split(',')
-            .map { it.trim().toComponent() })
-    }
-    else {
-        println(this)
-        Number(toIntOrNull()!!)
-    }
+    val str = (if(startsWith(',')) substring(1) else this).trim()
+    return if(str == "[]") Composite(listOf())
+    else if(str.startsWith('[')) Composite(buildList(0) {
+        var openBracketCount = 0
+        val iter = str.substring(1).iterator()
+        var char = iter.next()
+        var strComponent = ""
+        while (openBracketCount != 0 || char != ']') {
+            when (char) {
+                '[' -> openBracketCount++
+                ']' -> openBracketCount--
+                ',' -> if (openBracketCount == 0) {
+                    add(strComponent.toComponent())
+                    strComponent = ""
+                }
+            }
+            strComponent += char
+            char = iter.next()
+        }
+        add(strComponent.toComponent())
+    })
+    else Number(str.toIntOrNull()!!)
 }
-
-val pairs: List<List<Component>> = generateSequence(::readLine)
-    .filter { it.isNotEmpty() }
-    .map{ it.toComponent() }
-    .chunked(2).toList()
-
-println("AAA")
-
-val result = 0//pairs.filter { (o1, o2) -> o1 < o2 }.mapIndexed{ i, _ -> i + 1 }
-println("Part1: $result")
