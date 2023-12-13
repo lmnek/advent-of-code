89a0296a517e35bcddd6b412050dbf5f99b7e707
Krystof Gartner <krystofgartner@proton.me>
2023-12-14 01:32:23 +0800

    Updated template.hs

--- 2023/template.hs
+++ 2023/template.hs
@@ -1,26 +1,14 @@
+import AOCUtils (run)
 import Text.Parsec
 import Text.Parsec.String (Parser)
-import Control.Monad (forM_)
 
 inputFiles1 = [ "x_1" , "x_2" ]
 inputFiles2 = inputFiles1
 
 main :: IO() 
 main = do 
-    forM_ inputFiles1 $ doProblemsOnFile $ doProblem parseInput solve1 1 
-    forM_ inputFiles2 $ doProblemsOnFile $ doProblem parseInput solve2 2
-
-doProblemsOnFile doFunc fileName = do
-    input <- readFile $ "data/"++fileName
-    doFunc input
-
-doProblem :: Show b => Parser a -> (a -> b) -> Int -> String -> IO()
-doProblem parseData solve idx input = do
-    let str = case parse parseData "" input  of
-            Left err -> "Parsing error: " ++ show err
-            Right parsedData -> show idx ++ ": " ++ solution 
-                where solution = show $ solve parsedData
-    putStrLn str
+    run inputFiles1 parseInput solve1 1 
+    run inputFiles2 parseInput solve2 2 
 
 -- DATA TYPES / CLASSES --------------
 
